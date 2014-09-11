d752dba16b0c407712eed3987ac133023f4ed4c6
Cosimo Ranieri <co.ranieri@gmail.com>
2014-09-11 10:51:54 +0200

    add table view

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,5 +2,8 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
+
+    @table = UITableView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.addSubview(@table)
   end
 end
