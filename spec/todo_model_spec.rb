73449addcf9863e166a4a8cead2013291a6d006f
Cosimo Ranieri <co.ranieri@gmail.com>
2014-09-11 12:24:49 +0200

    add validation

--- spec/todo_model_spec.rb
+++ spec/todo_model_spec.rb
@@ -7,10 +7,15 @@ describe "Todo Model" do
     Object.const_defined?('Todo').should.be.true
   end
 
-  # it 'has a name, description, a due date and whether is done or not' do
-  #   @todo.should.respond_to :name
-  #   @todo.should.respond_to :description
-  #   @todo.should.respond_to :due_date
-  #   @todo.should.respond_to :done
-  # end
+  it 'has a name, description, a due date and whether is done or not' do
+    @todo.should.respond_to :name
+    @todo.should.respond_to :description
+    @todo.should.respond_to :due_date
+    @todo.should.respond_to :done
+  end
+
+  it 'is invalid without a name' do
+    @todo.name = nil
+    @todo.should.not.be.valid
+  end
 end
