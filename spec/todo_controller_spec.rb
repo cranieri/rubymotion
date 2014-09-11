be8077599aab8452fb15726cdd9f0076d5116e55
Cosimo Ranieri <co.ranieri@gmail.com>
2014-09-11 12:47:25 +0200

    add todo controller

--- spec/todo_controller_spec.rb
+++ spec/todo_controller_spec.rb
@@ -1,5 +1,33 @@
 describe "Todo Controller" do
+
+  tests TodoController
+  before do
+    @now = NSDate.new
+    @todo = Todo.create :name => "Buy Milk",
+                        :details => "We need some Milk",
+                        :due_date => @now
+    @controller = TodoController.new(@todo)
+
+    @form = @controller.instance_variable_get("@form")
+    @name_row = @form.sections[0].rows[0]
+    @details_row = @form.sections[0].rows[1]
+    @due_date_row = @form.sections[0].rows[2]
+    @done_row = @form.sections[0].rows[3]
+  end
+
+  def controller
+    @controller
+  end
+
   it 'exists' do
     Object.const_defined?('TodoController').should.be.true
+  end
+
+  it 'displays a Todo\'s details' do
+    @name_row.value.should.equal 'Buy Milk'
+    @details_row.value.should.equal 'We need some Milk'
+    @due_date_row.object.date_value.hour.should.equal @now.hour
+    @due_date_row.object.date_value.min.should.equal @now.min
+    @done_row.value.should.equal false
   end
 end
