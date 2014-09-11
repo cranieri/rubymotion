20e0d9814faaf674e67523599a1b323a2451ed5a
Cosimo Ranieri <co.ranieri@gmail.com>
2014-09-11 12:55:28 +0200

    new stuff

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -20,4 +20,11 @@ class TodosController < UITableViewController
     cell.textLabel.text = @data[indexPath.row].name
     cell
   end
+
+  def tableView(tableView, didSelectRowAtIndexPath: indexPath)
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    todo = @data[indexPath.row]
+    todo_controller = TodoController.new(todo)
+    self.navigationController.pushViewController(todo_controller, animated: true)
+  end
 end
